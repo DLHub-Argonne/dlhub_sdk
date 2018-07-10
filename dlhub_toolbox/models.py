a8a9eefad1fa20ba0169d4f7b1b5bc2fd99ab3a1
Logan Ward <ward.logan.t@gmail.com>
2018-07-10 09:56:25 -0500

    Added a class for marking up tabular data

--- dlhub_toolbox/models.py
+++ dlhub_toolbox/models.py
@@ -1,7 +1,14 @@
 from itertools import zip_longest
+import pandas as pd
+import os
 
 
 class Dataset:
+    """Base class for describing a dataset
+
+    The Dataset class and any of its subclasses contain opreations 
+    for describing what a dataset is and how to use it. 
+    """
 
     def __init__(self):
         """
@@ -40,5 +47,129 @@ class Dataset:
         return self
 
     def to_dict(self):
-        """Render the dataset to a JSON description"""
+        """Render the dataset to a JSON description
+        
+        Returns:
+            (dict) A description of the dataset in a form suitable for download"""
         return {"datacite": {"creators": self.authors, "title": self.title}}
+
+    def list_files(self): 
+        """Provide a list of files associated with this dataset. This list
+        should contain all of the files necessary to recreate the dataset.
+
+        Returns:
+            ([string]) list of file paths"""
+        raise NotImplementedError()
+
+
+class TabularDataset(Dataset):
+    """Read a dataset stored as a single file in a tabular format.
+    
+    Will read in the names of the columns, and allow users to associate
+    column names with descriptions of the data provided.
+    
+    This class is compatible with any data format readable by the Pandas
+    library. See the list of `read functions in Pandas<https://pandas.pydata.org/pandas-docs/stable/io.html>`_"""
+
+    def __init__(self, path, format="csv", read_kwargs={}):
+        """Initialize the description of a tabular dataset
+
+        Args:
+            path (string): Path to dataset
+            format (string): Format of the dataset. We support all of the 
+                read operations of Pandas (e.g., read_csv). Provide the format
+                of your dataset as the suffix for the Pandas read command (e.g.,
+                "csv" for "read_csv").
+            read_kwargs (dict): Any keyword arguments for the pandas read command
+        """
+        super(TabularDataset, self).__init__()
+        self.data = self.load_dataset(path, format, **read_kwargs)
+
+    def load_dataset(self, path, format, **kwargs):
+        """Load in a dataset to get some high-level descriptions of it
+
+        Args:
+        path (string): Path to dataset
+        format (string): Format of the dataset. We support all of the 
+            read operations of Pandas (e.g., read_csv). Provide the format
+            of your dataset as the suffix for the Pandas read command (e.g.,
+            "csv" for "read_csv").
+        **kwargs (dict): arguments for the Pandas read function
+        """
+        self.path = path
+        self.format = format
+        self.read_kwargs = kwargs
+
+        # Read in the data
+        read_fun = getattr(pd, 'read_{}'.format(format))
+        data = read_fun(path, **kwargs)
+        self.columns = dict((c,{'name': c}) for c in data.columns)
+
+        # Zero out the input and output columns
+        self.inputs = []
+        self.labels = []
+
+    def annotate_column(self, column_name, description=None, data_type=None, units=None):
+        """Provide documentation about a certain column within a dataset
+
+        Args:
+            column_name (string): Name of a column
+            description (string): Longer description of a column
+            data_type (string): Short description of the data type
+            units (string): Units for the columns data (if applicable)
+        """
+        self._check_column_name(column_name)
+        if description is not None:
+            self.columns[column_name]['description'] = description
+        if data_type is not None:
+            self.columns[column_name]['data_type'] = data_type
+        if units is not None:
+            self.columns[column_name]['units'] = units
+        return self
+
+    def _check_column_name(self, column_name):
+        if column_name not in self.columns:
+            raise ValueError('No such column {}'.format(column_name))
+
+    def mark_inputs(self, column_names):
+        """Mark which columns are inputs to a model
+
+        Args:
+            column_names ([string]): Names of columns
+        """
+        for c in column_names:
+            self._check_column_name(c)
+        self.inputs = column_names
+        return self
+ 
+    def mark_labels(self, column_names):
+        """Mark a column as label
+
+        Args:
+            column_names ([string]): Names of columns
+        """
+        for c in column_names:
+            self._check_column_name(c)
+        self.labels = column_names
+        return self
+
+    def list_files(self):
+        return [self.path]
+
+    def to_dict(self):
+        output = super(TabularDataset, self).to_dict()
+
+        dataset_block = {'path': os.path.abspath(self.path)}
+        # Add the format description
+        dataset_block['format'] = self.format
+        dataset_block['read_options'] = self.read_kwargs
+
+        # Add the column descriptions
+        dataset_block['columns'] = list(self.columns.values())
+        dataset_block['inputs'] = self.inputs
+        dataset_block['labels'] = self.labels
+
+        # Add to the full dataset
+        output['dataset'] = dataset_block
+
+        return output
--- dlhub_toolbox/tests/test_models.py
+++ dlhub_toolbox/tests/test_models.py
@@ -1,9 +1,13 @@
-from dlhub_toolbox.models import Dataset
+from dlhub_toolbox.models import Dataset, TabularDataset
 
 import unittest
+import json
+import os
 
 
 class TestModels(unittest.TestCase):
+
+    maxDiff=None
 
     def test_dataset(self):
         m = Dataset().set_authors(["Ward, Logan"], ["University of Chicago"])\
@@ -13,6 +17,24 @@ class TestModels(unittest.TestCase):
                                                       "affiliations": "University of Chicago"}],
                                         "title": "Example dataset"
                                         }})
+
+    def test_tabular_dataset(self):
+        data_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'test.csv'))
+        m = TabularDataset(data_path)
+        self.assertEqual({'x': {'name': 'x'}, 'y': {'name': 'y'}}, m.columns)
+
+        # Add some nonsense
+        m.set_title('Example dataset')
+        m.mark_inputs(['x'])
+        m.mark_labels(['y'])
+        m.annotate_column('x', description='Input variable', units='cm')
+        m.annotate_column('y', data_type='scalar')
+        self.assertEqual(m.to_dict(), {"datacite": {"title": "Example dataset",
+            "creators": []}, "dataset": {"path": data_path, "columns": [
+                {"name": "x", "description": "Input variable", "units": "cm"},
+                {"name": "y", "data_type": "scalar"}],
+                "inputs": ["x"], "labels": ["y"],
+                "format": "csv", "read_options": {}}})
 
 
 if __name__ == "__main__":
