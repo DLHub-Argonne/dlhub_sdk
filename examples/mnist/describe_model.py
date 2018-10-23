b1838391115ed9a15492f90d3d46312796f34973
Logan Ward <ward.logan.t@gmail.com>
2018-10-23 09:01:23 -0500

    Fixed MNIST example

--- examples/mnist/describe_model.py
+++ examples/mnist/describe_model.py
@@ -12,8 +12,8 @@ model_info.set_name("mnist_tiny_example")
 model_info.set_domains(["digit recognition"])
 
 #    Describe the outputs in more detail
-model_info.output['description'] = 'Probabilities of being 0-9'
-model_info.input['description'] = 'Image of a digit'
+model_info['servable']['methods']['run']['output']['description'] = 'Probabilities of being 0-9'
+model_info['servable']['methods']['run']['input']['description'] = 'Image of a digit'
 
 # Print out the result
 print('\n--> Model Information <--')
