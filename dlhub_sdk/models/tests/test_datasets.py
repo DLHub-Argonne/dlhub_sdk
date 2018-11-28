ce096b2a7a9fc59875847ac088647188f63d57ff
Logan Ward <ward.logan.t@gmail.com>
2018-11-28 14:00:54 -0600

    Test fix: Make sure to change to proper directory

--- dlhub_sdk/models/tests/test_datasets.py
+++ dlhub_sdk/models/tests/test_datasets.py
@@ -203,7 +203,16 @@ class TestModels(unittest.TestCase):
 
     def test_codemeta(self):
         m = TabularDataset()
-        m.read_codemeta_file()
+
+        # Read in the codemeta file
+        odir = os.getcwd()
+        try:
+            os.chdir(os.path.dirname(__file__))
+            m.read_codemeta_file()
+        finally:
+            os.chdir(odir)
+
+        # Check that it produces valid datacite
         validate_against_dlhub_schema(m['datacite'], 'datacite-v4.1')
 
 if __name__ == "__main__":
