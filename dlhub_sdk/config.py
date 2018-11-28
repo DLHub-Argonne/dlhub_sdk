788f58ccff058bb7e2647dfb122a7c0c6412a94b
Ryan <rchard@anl.gov>
2018-11-28 13:57:41 -0600

    Minor fix.

--- dlhub_sdk/config.py
+++ dlhub_sdk/config.py
@@ -154,7 +154,4 @@ def format_output(dataobject):
     :param dataobject:
     :return:
     """
-    if isinstance(dataobject, six.string_types):
-        safeprint(dataobject)
-    else:
-        safeprint(json.dumps(dataobject, indent=2, separators=(',', ': ')))
+    safeprint(json.dumps(dataobject, indent=2, separators=(',', ': ')))
