784ba96818f286068856d5aa0afafb2a42e543ec
Logan Ward <ward.logan.t@gmail.com>
2018-12-31 11:55:24 -0600

    Incremented version number

--- dlhub_sdk/version.py
+++ dlhub_sdk/version.py
@@ -1,6 +1,6 @@
 # single source of truth for package version,
 # see https://packaging.python.org/en/latest/single_source_version/
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 # app name to send as part of SDK requests
 app_name = "DLHub SDK v{}".format(__version__)
