b5e6e973793ee2bb9bfe788ac3d086d06ae351bf
Logan Ward <ward.logan.t@gmail.com>
2019-01-15 15:35:05 -0600

    Added jsonpickle to docs, attempt to resolve numpy header problem

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
                       "correct schema for DLHub, and discovering or using models "
                       "that other scientists have published."),
     install_requires=[
-        "pandas", "requests", "jsonschema", "globus_sdk", "configobj"
+        "pandas", "requests", "jsonschema", "globus_sdk", "configobj", "jsonpickle"
     ],
     python_requires=">=3.4",
     classifiers=[
