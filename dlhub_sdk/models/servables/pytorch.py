1ffd9721570269c067c221ea1931a119924eeff8
Ethan Truelove <etruelove@uchicago.edu>
2019-07-16 10:37:13 -0500

    basic pytorch support

---
+++ dlhub_sdk/models/servables/pytorch.py
@@ -0,0 +1,69 @@
+import torch
+import sys
+import io
+
+from dlhub_sdk.models.servables.python import BasePythonServableModel
+from dlhub_sdk.utils.types import compose_argument_block
+
+class TorchModel(BasePythonServableModel):
+    """Servable based on a Torch Model object.
+
+    Assumes that the model has been saved to a pt or a pth file"""
+
+    @classmethod
+    def create_model(cls, model_path, input_shape, output_shape):
+        output = super(TorchModel, cls).create_model('predict')
+
+        # Add model as a file to be sent
+        output.add_file(model_path, 'model')
+
+        # Get the model details
+        if model_path.endswith('.pt') or model_path.endswith('.pth'):
+            '''
+            model_module = __import__(module_name)
+            model_class = getattr(model_module, class_name)
+            model  = model_class()
+            model.load_state_dict(torch.load(model_path))
+            model.eval()
+            '''
+            model = torch.load(model_path)
+        else:
+            raise ValueError('File type for architecture not recognized')
+
+        # Get the inputs of the model
+        output['servable']['methods']['run']['input'] = output.format_layer_spec(input_shape)
+        output['servable']['methods']['run']['output'] = output.format_layer_spec(output_shape)
+        
+        output['servable']['model_summary'] = str(model)
+        output['servable']['model_type'] = 'Deep NN' 
+
+        # Add torch as a dependency
+        output.add_requirement('torch', torch.__version__)
+
+        #print(output['servable'])
+        return output
+
+    def format_layer_spec(self, layers):
+        """Make a description of a list of input or output layers
+
+        Args:
+            layers (tuple or [tuple]): Shape of the layers
+        Return:
+            (dict) Description of the inputs / outputs
+        """
+        if isinstance(layers, tuple):
+            return compose_argument_block("ndarray", "Tensor", shape=list(layers))
+        else:
+            return compose_argument_block("tuple", "Tuple of tensors",
+                                          element_types=[self.format_layer_spec(i) for i in layers])
+
+    def _get_handler(self):
+        return "torch.TorchServable"
+
+    def _get_type(self):
+        return "Torch Model"
+'''
+if __name__ == '__main__':
+    test = TorchModel()
+    test.create_model('mnist_cnn.pt', 'Net', 'Net', (20, 5, 1), (2, 1))
+'''
---
+++ dlhub_sdk/models/servables/tests/test_pytorch.py
@@ -0,0 +1,83 @@
+from datetime import datetime
+from tempfile import mkdtemp
+import shutil
+import os
+
+from unittest import TestCase
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import pprint as pprint
+
+from Net import Net
+
+from dlhub_sdk.models.servables.pytorch import TorchModel
+from dlhub_sdk.utils.schemas import validate_against_dlhub_schema
+from dlhub_sdk.version import __version__
+
+_year = str(datetime.now().year)
+
+def _make_simple_model():
+    model = Net()
+    return model
+
+class TestTorch(TestCase):
+    maxDiff = 4096
+
+    def test_torch_single_input(self):
+        # Make a Keras model
+        model = _make_simple_model()
+
+        # Save it to disk
+        tempdir = mkdtemp()
+        try:
+            model_path = os.path.join(tempdir, 'model.pt')
+            torch.save(model, model_path)
+
+            # Create a model
+            metadata = TorchModel.create_model(model_path, (2, 4), (3, 5))
+            metadata.set_title('Torch Test')
+            metadata.set_name('mlp')
+
+            output = metadata.to_dict()
+            self.assertEqual(output, {
+                "datacite": {"creators": [], "titles": [{"title": "Torch Test"}],
+                             "publisher": "DLHub", "publicationYear": _year,
+                             "identifier": {"identifier": "10.YET/UNASSIGNED",
+                                            "identifierType": "DOI"},
+                             "resourceType": {"resourceTypeGeneral": "InteractiveResource"},
+                             "descriptions": [],
+                             "fundingReferences": [],
+                             "relatedIdentifiers": [],
+                             "alternateIdentifiers": [],
+                             "rightsList": []},
+                "dlhub": {"version": __version__, "domains": [],
+                          "visible_to": ["public"],
+                          'type': 'servable',
+                          "name": "mlp", "files": {"model": model_path},
+                          "dependencies": {"python": {
+                              'torch': torch.__version__
+                          }}},
+                "servable": {"methods": {"run": {
+                    "input": {"type": "ndarray", "description": "Tensor", "shape": [2, 4]},
+                    "output": {"type": "ndarray", "description": "Tensor",
+                               "shape": [3, 5]}, "parameters": {},
+                    "method_details": {
+                        "method_name": "predict"
+                    }}},
+                    "type": "Torch Model",
+                    "shim": "torch.TorchServable",
+                    "model_type": "Deep NN",
+                    "model_summary": """Net(
+  (conv1): Conv2d(1, 20, kernel_size=(5, 5), stride=(1, 1))
+  (conv2): Conv2d(20, 50, kernel_size=(5, 5), stride=(1, 1))
+  (fc1): Linear(in_features=800, out_features=500, bias=True)
+  (fc2): Linear(in_features=500, out_features=10, bias=True)
+)"""}})
+
+            # Validate against schema
+            validate_against_dlhub_schema(output, 'servable')
+        finally:
+            shutil.rmtree(tempdir)
