709eade0c0ccf4f6b1644e626997e8c491011ce1
Logan Ward <ward.logan.t@gmail.com>
2019-09-11 12:01:42 -0500

    Increment version
    Fixed problems with installation on systems that already have jsonschema

--- dlhub_sdk/version.py
+++ dlhub_sdk/version.py
@@ -1,6 +1,6 @@
 # single source of truth for package version,
 # see https://packaging.python.org/en/latest/single_source_version/
-__version__ = "0.8.2"
+__version__ = "0.8.3"
 
 # app name to send as part of SDK requests
 app_name = "DLHub SDK v{}".format(__version__)
