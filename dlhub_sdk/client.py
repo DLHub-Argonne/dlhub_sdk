e923f979f1749bb184a6549130d7a5a00e918aee
Ben Blaiszik <blaiszik@uchicago.edu>
2020-06-02 13:23:11 -0500

    set force_login to force_login variable

--- dlhub_sdk/client.py
+++ dlhub_sdk/client.py
@@ -79,7 +79,7 @@ class DLHubClient(BaseClient):
             dlh_authorizer = auth_res["dlhub"]
             fx_authorizer = auth_res[fx_scope]
             self._search_client = auth_res["search"]
-            self._fx_client = FuncXClient(force_login=True,
+            self._fx_client = FuncXClient(force_login=force_login,
                                           fx_authorizer=fx_authorizer,
                                           no_local_server=kwargs.get("no_local_server", True),
                                           no_browser=kwargs.get("no_browser", True),
