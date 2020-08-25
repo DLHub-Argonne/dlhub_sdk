67897ced95d96ef8f8fd4a006bd988b01a8a650b
Logan Ward <ward.logan.t@gmail.com>
2020-08-25 12:10:06 -0500

    Only install TF once
    Otherwise I get some version issues with keras libraries

--- travis/install.sh
+++ travis/install.sh
@@ -7,14 +7,17 @@ pip install --upgrade pip setuptools wheel
 pip install -e .
 pip install coveralls flake8
 
+# Adding keras and TF1 for some tests
+if [ -n "$KERAS_VERSION" ]; then
+  pip install keras==$KERAS_VERSION "tensorflow<2"
+else
+  # Otherwise, use TF2
+  pip install "tensorflow>2"
+fi
+
 # Install the packages required for tests
 pip install -r test-requirements.txt
 pip install -r example-requirements.txt
-
-# Adding keras, an optional dependency
-if [ ! -z "$KERAS_VERSION" ]; then
-  pip install keras==$KERAS_VERSION "tensorflow<2"
-fi
 
 # Manually-specifying the version of scikit-learn
 if [ ! -z "$SKLEARN_VERSION" ]; then
