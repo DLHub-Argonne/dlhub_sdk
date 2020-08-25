cd2da0cb76f1b91e44101a0c2d451a6de144d070
Logan Ward <ward.logan.t@gmail.com>
2020-08-25 12:54:17 -0500

    Update to verson 0.9.0

--- dlhub_sdk/version.py
+++ dlhub_sdk/version.py
@@ -1,6 +1,6 @@
 # single source of truth for package version,
 # see https://packaging.python.org/en/latest/single_source_version/
-__version__ = "0.8.4"
+__version__ = "0.9.0"
 
 # app name to send as part of SDK requests
 app_name = "DLHub SDK v{}".format(__version__)
