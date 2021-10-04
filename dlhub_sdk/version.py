3e5c672a48c433717237241edcd7da3cf1333a13
ascourtas <ascourtas@gmail.com>
2021-10-04 12:49:43 -0400

    update version to 0.9.8

--- dlhub_sdk/version.py
+++ dlhub_sdk/version.py
@@ -1,6 +1,6 @@
 # single source of truth for package version,
 # see https://packaging.python.org/en/latest/single_source_version/
-__version__ = "0.9.7"
+__version__ = "0.9.8"
 
 # app name to send as part of SDK requests
 app_name = "DLHub SDK v{}".format(__version__)
