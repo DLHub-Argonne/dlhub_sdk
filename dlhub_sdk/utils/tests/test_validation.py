eb7470df8eb07db1612e7d96d14af479d0bd0925
isaac-darling <61167692+isaac-darling@users.noreply.github.com>
2022-07-01 14:53:43 -0500

    add tests to increase coverage

--- dlhub_sdk/utils/tests/test_validation.py
+++ dlhub_sdk/utils/tests/test_validation.py
@@ -7,22 +7,26 @@ from dlhub_sdk.utils.validation import validate
 def test_validation() -> None:
     metadata = [
         {"type": "integer"},
+        {"type": "integer"},
         {"type": "float"},
         {"type": "boolean"},
         {"item_type": {"type": "integer"}, "type": "list"},
         {"element_types": [{"type": "integer"}, {"type": "integer"}, {"type": "string"}], "type": "tuple"},
         {"properties": {"a": {"type": "integer"}, "b": {"type": "integer"}, "c": {"type": "integer"}}, "type": "dict"},
         {"shape": ["None", "2"], "item_type": {"type": "integer"}, "type": "ndarray"},
+        {"shape": ["1"], "item_type": {"properties": {"a": {"type": "integer"}}, "type": "dict"}, "type": "ndarray"},
         {"item_type": {"properties": {"a": {"item_type": {"type": "integer"}, "type": "list"}}, "type": "dict"}, "type": "list"}
     ]
     inputs = [
         1,
+        True,
         1.23,
         False,
         [1, 2, 3],
         (1, 2, "hello"),
         {"a": 1, "b": 2, "c": 3},
         array([[1, 2], [3, 4]]),
+        array([{"a": 1}]),
         [{"a": [1, 2, 3, 4]}]
     ]
 
@@ -31,7 +35,26 @@ def test_validation() -> None:
 
 
 def test_validation_errs() -> None:
+    with raises(ValueError):
+        validate(1, {"type": "nonsense"})
+
     with raises(TypeError):
-        validate("1", {"type": "integer"})
+        validate(None, {"type": "integer"})
+
+    with raises(TypeError):
+        validate({"a": [1, 2, 3, 4.]}, {"properties": {"a": {"item_type": {"type": "integer"}, "type": "list"}}, "type": "dict"})
+
+    with raises(ValueError):
+        validate((1, 2, "hello"), {"element_types": [{"type": "integer"}, {"type": "integer"}], "type": "tuple"})
+
+    with raises(ValueError):
+        validate(array([[1, 2], [3, 4]]), {"shape": ["None"], "type": "ndarray"})
+
     with raises(ValueError):
         validate(array([1, 2]), {"shape": ["1"], "type": "ndarray"})
+
+    with raises(ValueError):
+        validate({"a": 1, "b": 2, "c": 3}, {"properties": {"a": {"type": "integer"}, "b": {"type": "integer"}}, "type": "dict"})
+
+    with raises(ValueError):
+        validate({"a": 1, "b": 2}, {"properties": {"a": {"type": "integer"}, "b": {"type": "integer"}, "c": {"type": "integer"}}, "type": "dict"})
